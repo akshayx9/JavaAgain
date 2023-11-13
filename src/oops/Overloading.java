cb2b674011f1f5cc91becd1ebb9c5fc905b36efc
akshayx9 <akshaysinghx9@gmail.com>
2023-11-13 19:20:17 +0530

    third commit

---
+++ src/oops/Overloading.java
@@ -0,0 +1,23 @@
+package oops;
+
+public class Overloading {
+    
+    public void m1(){
+        System.out.println("m1() with no-args");
+    }
+
+    public void m1(int x){
+        System.out.println("m1() with int args");
+    }
+
+    public void m1(float f){
+        System.out.println("m1() with float args");
+    }
+
+    public static void main(String[] args) {
+        Overloading o = new Overloading();
+        o.m1();
+        o.m1(100);
+        o.m1(34.5f);
+    }
+}
