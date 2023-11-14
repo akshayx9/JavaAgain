d22cfa6b58ad0e3be401a227dcd2956d0e75a27e
Akshay <akshaysinghx9@gmail.com>
2023-11-15 00:25:35 +0530

    ninth commit

---
+++ src/Assignments/Assignment_3/EvenOdd.java
@@ -0,0 +1,20 @@
+package Assignments.Assignment_3;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        
+        Scanner s=new Scanner(System.in);
+        
+        System.out.println("Enter a number:");
+        int a=s.nextInt();
+        s.close();
+
+        if(a/2==0){
+            System.out.println("The number "+a+" is even");
+        }else{
+            System.out.println("The number "+a+" is odd");
+        }
+    }
+}
