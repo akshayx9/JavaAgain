3c8213ad8672d89b0a434818dd477ed2613049e3
Akshay <akshaysinghx9@gmail.com>
2023-11-28 19:48:38 +0530

    twenty-second commit

---
+++ src/Collections/Enumerations.java
@@ -0,0 +1,24 @@
+package Collections;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Enumerations {
+    public static void main(String[] args) {
+
+        Vector<Object> v = new Vector<>();
+        for (int i = 1; i <= 10; i++) {
+            v.addElement(i);
+        }
+
+        System.out.println(v);
+        System.out.println("*******************");
+        Enumeration<Object> e = v.elements();
+        while (e.hasMoreElements()) {
+            Integer i1 = (Integer) e.nextElement();
+            if (i1 % 2 == 0) {
+                System.out.println(i1);
+            }
+        }
+    }
+}
---
+++ src/Collections/Iterators.java
@@ -0,0 +1,28 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Iterators {
+    public static void main(String[] args) {
+
+        ArrayList<Object> list = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            list.add(i);
+        }
+
+        System.out.println(list);
+        System.out.println("*******************");
+
+        Iterator<Object> itr = list.iterator();
+        while (itr.hasNext()) {
+            Integer i1 = (Integer) itr.next();
+            if (i1 % 2 == 0) {
+                System.out.println(i1);
+            } else {
+                itr.remove();
+            }
+        }
+        System.out.println("Final List::" + list);
+    }
+}
